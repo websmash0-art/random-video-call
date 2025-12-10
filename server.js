38836667a01e70b5e55be9ed7c24a17667a90e34
websmash0-art <websmash0@gmail.com>
2025-12-10 13:32:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
@@ -10,54 +9,53 @@ const wss = new WebSocket.Server({ server });
 
 app.use(express.static('public'));
 
-let waiting = null; // stores one waiting client
+let waiting = null; // waiting user
 
 wss.on('connection', (ws) => {
   ws.id = uuidv4();
-  ws.isWaiting = false;
 
   ws.on('message', (msg) => {
     let data;
-    try { data = JSON.parse(msg); } catch (e) { return; }
+    try { data = JSON.parse(msg); } catch(e){ return; }
 
-    if (data.type === 'join') {
-      if (waiting && waiting !== ws && waiting.readyState === WebSocket.OPEN) {
-        const room = 'random_' + uuidv4().split('-')[0];
-
+    if(data.type === 'join'){
+      if(waiting && waiting !== ws && waiting.readyState === WebSocket.OPEN){
+        const room = uuidv4().split('-')[0];
         ws.partner = waiting;
         waiting.partner = ws;
-
-        ws.send(JSON.stringify({ type: 'room', room }));
-        waiting.send(JSON.stringify({ type: 'room', room }));
-
+        ws.send(JSON.stringify({ type:'partner_found', room }));
+        waiting.send(JSON.stringify({ type:'partner_found', room }));
         waiting = null;
-        console.log('Paired', ws.id, ws.partner.id, 'room', room);
       } else {
         waiting = ws;
-        ws.isWaiting = true;
-        ws.send(JSON.stringify({ type: 'waiting' }));
+        ws.send(JSON.stringify({ type:'waiting' }));
       }
     }
 
-    if (data.type === 'leave') {
-      if (ws.partner && ws.partner.readyState === WebSocket.OPEN) {
-        ws.partner.send(JSON.stringify({ type: 'partner_left' }));
+    if(data.type === 'signal'){
+      if(ws.partner && ws.partner.readyState === WebSocket.OPEN){
+        ws.partner.send(JSON.stringify({ type:'signal', signal: data.signal }));
+      }
+    }
+
+    if(data.type === 'leave'){
+      if(ws.partner && ws.partner.readyState === WebSocket.OPEN){
+        ws.partner.send(JSON.stringify({ type:'partner_left' }));
         ws.partner.partner = null;
       }
-      if (waiting === ws) waiting = null;
-      ws.isWaiting = false;
+      if(waiting === ws) waiting = null;
       ws.partner = null;
     }
   });
 
-  ws.on('close', () => {
-    if (ws.partner && ws.partner.readyState === WebSocket.OPEN) {
-      ws.partner.send(JSON.stringify({ type: 'partner_left' }));
+  ws.on('close', ()=>{
+    if(ws.partner && ws.partner.readyState === WebSocket.OPEN){
+      ws.partner.send(JSON.stringify({ type:'partner_left' }));
       ws.partner.partner = null;
     }
-    if (waiting === ws) waiting = null;
+    if(waiting === ws) waiting = null;
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Server running on http://localhost:' + PORT));
+server.listen(PORT, ()=>console.log('Server running on http://localhost:'+PORT));
